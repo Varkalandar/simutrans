a23650ee84d460d3b9cc4064768b0c4fef5f7e0e
Hj. Malthaner <62077919+Varkalandar@users.noreply.github>
2022-01-10 15:17:45 +0100

    NEW: Silver UI theme added.

---
+++ themes.src/silver/build.sh
@@ -0,0 +1,6 @@
+rm -rf *.pak
+../../../../makeobj pak silver.pak standard.dat
+../../../../makeobj pak silver-large.pak standard-large.dat
+mv *.pak ../../../../simutrans/themes
+cp *.tab ../../../../simutrans/themes
+
